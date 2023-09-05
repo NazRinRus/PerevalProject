650b27ce67e8b6c652dc6f25030f85cf8100ea22
NazRinRus <NazRinRus@gmail.com>
2023-09-05 21:47:41 +0300

    Pereval БД разработана, написание views, serializers

--- pereval/serializers.py
+++ pereval/serializers.py
@@ -6,3 +6,19 @@ class PerevalAddedSerializer(serializers.ModelSerializer):
         model = PerevalAdded
         fields = ('status', 'beautyTitle', 'title', 'other_titles', 'connect', 'add_time', 'images', 'coord_id',
                   'winter_lvl', 'summer_lvl', 'autumn_lvl', 'spring_lvl', 'author')
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('mail', 'phone', 'name', 'surname', 'otch')
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        fields = ('name', 'photos')
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = ('latitude', 'longitude', 'height')
+
