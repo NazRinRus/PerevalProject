82428ace63645102fa69d5ff22efb000536e9ef4
NazRinRus <NazRinRus@gmail.com>
2023-09-10 10:51:34 +0300

    Pereval БД разработана, простейшие views, serializers имеются, разрабатываю submitData. Изменил структуру БД, таблицы Перевал и Фотографии не содержат ссылок друг на друга, ссылки хранятся в отдельной таблице Pereval_Images. Пароль, пользователь и хост выведены и сетингс в файл конфиг

--- pereval/models.py
+++ pereval/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
 
-def get_image_path(instance, file):
+def get_image_path(instance, file): # прописываю путь сохранения изображений, у каждой записи PerevalAdded своя папка
     return f'photos/pereval-{instance.pereval.id}/{file}'
 
+# Активность - способ прохождения локации, вывел в список кортежей
 ACTIVITIES = [
     ('foot', 'пеший'),
     ('bike', 'велосипед'),
@@ -11,6 +12,7 @@ ACTIVITIES = [
     ('motorbike', 'мотоцикл'),
 ]
 
+# Вид локации, вывел в список кортежей
 BEAUTYTITLE = [
     ('poss', 'перевал'),
     ('mountain_peak', 'горная вершина'),
@@ -18,7 +20,7 @@ BEAUTYTITLE = [
     ('plateau', 'плато'),
 ]
 
-
+# статус добавленной записи пользователя, списком кортежей
 STATUS = [
     ('new', 'новый'),
     ('pending', 'на модерации'),
@@ -26,6 +28,7 @@ STATUS = [
     ('rejected', 'не принят'),
 ]
 
+# Уровень сложности прохождения локации, списком кортежей
 LEVELS = [
     ('', 'не указано'),
     ('1A', '1a'),
@@ -36,10 +39,9 @@ LEVELS = [
     ('3В', '3б'),
     ]
 
-
+# Собственный класс, хранящий информацию о пользователях
 class Users(models.Model):
-
-    mail = models.EmailField('почта', unique=True)
+    mail = models.EmailField('почта', unique=True)# поле электронной почты, оно уникально, по нему проверяю ункикальность пользователей
     phone = models.CharField('телефон', max_length=15)
     name = models.CharField('имя', max_length=30)
     surname = models.CharField('фамилия', max_length=30)
@@ -48,9 +50,8 @@ class Users(models.Model):
     def __str__(self):
         return f'{self.surname}'
 
-
+# Координаты локации хранятся в отдельной таблице
 class Coords(models.Model):
-
     latitude = models.FloatField('широта', max_length=9, blank=True)
     longitude = models.FloatField('долгота', max_length=9, blank=True)
     height = models.IntegerField('высота', blank=True)
@@ -58,28 +59,32 @@ class Coords(models.Model):
 
 
 
-
+# Класс, хранящий данные, переданные пользователем
 class PerevalAdded(models.Model):
+    status = models.CharField(choices=STATUS, max_length=25, default='new') #статус нового сообщения, по умолчанию Новое
+    beautyTitle = models.CharField('тип', choices=BEAUTYTITLE, max_length=50)#тип локации - перевал, ущелье и т.д. списком
+    title = models.CharField('название', max_length=50, blank=True)# название локации
+    other_titles = models.CharField('иные названия', max_length=50)# описание локации
+    connect = models.CharField('соединение', max_length=250)# какие локации соединяет (применимо к перевалу)
+    add_time = models.DateTimeField(default=timezone.now, editable=False)#дата/время создания записи (не понял пользователь вручную создает или автоматическое поле при добавлении в БД)
+    coord_id = models.OneToOneField(Coords, on_delete=models.CASCADE)# ссылка на объект с координатами локации. Зачем если связь один к одному?
+    winter = models.CharField('зима', max_length=2, choices=LEVELS)# уровень сложности прохождения локации зимой
+    summer = models.CharField('лето', max_length=2, choices=LEVELS)# уровень сложности прохождения локации летом
+    autumn = models.CharField('осень', max_length=2, choices=LEVELS)# уровень сложности прохождения локации осенью
+    spring = models.CharField('весна', max_length=2, choices=LEVELS)# уровень сложности прохождения локации весной
+    author = models.ForeignKey(Users, on_delete=models.CASCADE)# автор статьи - ссылка на объект пользователей
 
-    status = models.CharField(choices=STATUS, max_length=25, default='new')
-    beautyTitle = models.CharField('тип', choices=BEAUTYTITLE, max_length=50)
-    title = models.CharField('название', max_length=50, blank=True)
-    other_titles = models.CharField('иные названия', max_length=50)
-    connect = models.CharField('соединение', max_length=250)
-    add_time = models.DateTimeField(default=timezone.now, editable=False)
-    coord_id = models.OneToOneField(Coords, on_delete=models.CASCADE)
-    winter = models.CharField('зима', max_length=2, choices=LEVELS)
-    summer = models.CharField('лето', max_length=2, choices=LEVELS)
-    autumn = models.CharField('осень', max_length=2, choices=LEVELS)
-    spring = models.CharField('весна', max_length=2, choices=LEVELS)
-    author = models.ForeignKey(Users, on_delete=models.CASCADE)
-
-
+# класс фотографии добавленные пользователем
 class Images(models.Model):
-    name = models.CharField(max_length=50)
-    photos = models.ImageField('Фото', upload_to=get_image_path, blank=True, null=True)
-    pereval = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE, default=0)
+    name = models.CharField(max_length=50)# название фотографии
+    photos = models.ImageField('Фото', upload_to=get_image_path, blank=True, null=True)# объект фотографии
 
+#таблица объединяющая объекты таблиц Перевал и Фотографии
+class Pereval_Images(models.Model):
+    pereval = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE, default=0)  # ссылка на объект локации
+    images = models.ForeignKey(Images, on_delete=models.CASCADE, default=0)  # ссылка на объект фотографии
+
+# данные таблицы были в ТЗ не понял зачем, видимо для дальнейшей работы, пока не используются
 class PerevalAreas(models.Model):
 
     id_parent = models.IntegerField(blank=True)
--- pereval/views.py
+++ pereval/views.py
@@ -4,40 +4,41 @@ from .models import *
 from .serializers import *
 from  rest_framework import viewsets
 
-class PerevalAddedListAPIView(generics.ListCreateAPIView):
+# сериализеры для работы с данными таблиц, просмотр, добавление, удаление
+class PerevalAddedListAPIView(generics.ListCreateAPIView):# сериализер для работы с таблицей перевалов, просмотр, добавление
     queryset = PerevalAdded.objects.all()
     serializer_class = PerevalAddedSerializer
 
-class PerevalAddedDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class PerevalAddedDetailAPIView(generics.RetrieveUpdateDestroyAPIView):# универсальный сериализер для работы с объектом перевалов, редактирование, удаление
     queryset = PerevalAdded.objects.all()
     serializer_class = PerevalAddedSerializer
 
-class UsersListAPIView(generics.ListCreateAPIView):
+class UsersListAPIView(generics.ListCreateAPIView):# универсальный сериализер для работы с таблицей пользователей, просмотр, добавление
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
 
-class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):# универсальный сериализер для работы с объектом пользователей, редактирование, удаление
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
 
 
-class ImagesListAPIView(generics.ListCreateAPIView):
+class ImagesListAPIView(generics.ListCreateAPIView):# сериализер для работы с таблицей фотографий, просмотр, добавление
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
 
-class ImagesDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class ImagesDetailAPIView(generics.RetrieveUpdateDestroyAPIView):# универсальный сериализер для работы с объектом фотографии, редактирование, удаление
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
 
-class CoordsListAPIView(generics.ListCreateAPIView):
+class CoordsListAPIView(generics.ListCreateAPIView):# сериализер для работы с таблицей координат, просмотр, добавление
     queryset = Coords.objects.all()
     serializer_class = CoordsSerializer
 
-class CoordsDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class CoordsDetailAPIView(generics.RetrieveUpdateDestroyAPIView):# универсальный сериализер для работы с объектом координат, редактирование, удаление
     queryset = Coords.objects.all()
     serializer_class = CoordsSerializer
 
-# Классы для реализации submitData
+# Классы для реализации submitData, так же универсальные, можно добавлять, редактировать, удалять, просматривать, доступны по адресу api/v2/
 class PerevalAddedViewSet(viewsets.ModelViewSet):
     queryset = PerevalAdded.objects.all()
     serializer_class = PerevalAddedSerializer
